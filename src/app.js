1ebb9c28a98e7528cd8ef20095e6414b6f0fb664
Abhinav Pandey <abhinavpandey1230@gmail.com>
2025-01-15 22:40:26 +0530

    Update Swagger and Welcome Route

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-/**
- *
- * This file configures the Express application, setting up middleware and routes.
- */
-
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const { errorHandler } = require('./middlewares/error.middleware'); // Import our global error handler
-const routes = require('./routes'); // Import the central router
+const { errorHandler } = require('./middlewares/error.middleware');
+const routes = require('./routes');
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
+// Load the openapi.yaml file
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/openapi.yaml'));
 
 const app = express();
 
@@ -16,8 +18,16 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to Event Management API. Please refer to \'/api-docs\' for Swagger Documentations');
+});
+
 // Mount all routes under /api
 app.use('/api', routes);
+
+// Served Swagger UI at /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // At the end: use our error handling middleware
 app.use(errorHandler);
